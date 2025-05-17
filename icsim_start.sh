b291e7cac210d7d24e61df8802d7f8345f533a69
V0lk3n <22322762+V0lk3n@users.noreply.github.com>
2025-05-17 14:49:05 +0200

    Update icsim_start.sh

--- icsim_start.sh
+++ icsim_start.sh
@@ -2,26 +2,26 @@
 
 CAN_IFACE="$1"
 
-if [ -z "$CAN_IFACE" ]; then
-    CAN_IFACE="vcan0"
-fi
-
+# ICSim Display (Display :1, VNC 5900, noVNC 6080)
 export DISPLAY=:1
-
-# launch virtual display
 Xvfb :1 -screen 0 692x350x16 &
-
-# launch window manager
 sleep 5
 fluxbox &
-
-# launch VNC Server
 sleep 2
 x11vnc -display :1 -nopw -forever -bg -rfbport 5900
-
 cd /opt/car_hacking/ICSim/builddir
 ./icsim "$CAN_IFACE" &
 
-# run noVNC proxy
+# Controls Display (Display :2, VNC 5901, noVNC 6081)
+export DISPLAY=:2
+Xvfb :2 -screen 0 692x350x16 &
+sleep 5
+fluxbox &
+sleep 2
+x11vnc -display :2 -nopw -forever -bg -rfbport 5901
+./controls "$CAN_IFACE" &
+
+# noVNC proxies
 cd /opt/noVNC
-./utils/novnc_proxy --vnc localhost:5900 --listen 6080
+./utils/novnc_proxy --vnc localhost:5900 --listen 6080 &
+./utils/novnc_proxy --vnc localhost:5901 --listen 6081 &
